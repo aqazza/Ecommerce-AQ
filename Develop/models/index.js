9ea9db50d8fecd7d7f4a08773592eb8649e538cf
Anas Qazza <anasqazza@gmail.com>
2023-01-07 23:44:18 -0800

    Added code to the category routes and ran npm i's

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,31 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongtoMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
 
+  as: product_tag,
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongtoMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false, // can have many of the same tags
+  },
+
+  as: product_tag,
+});
 
 module.exports = {
   Product,
