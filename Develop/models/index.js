279706c88a5e702d849ed80a723ee819469655c7
Anas Qazza <anasqazza@gmail.com>
2023-01-09 17:30:31 -0800

    factored all js files in models folder

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,9 +5,13 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongtoMany(Tag, {
   through: {
@@ -15,7 +19,7 @@ Product.belongtoMany(Tag, {
     unique: false,
   },
 
-  as: product_tag,
+  foreignKey: "product_id",
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongtoMany(Product, {
@@ -24,7 +28,7 @@ Tag.belongtoMany(Product, {
     unique: false, // can have many of the same tags
   },
 
-  as: product_tag,
+  foreignKey: "tag_id",
 });
 
 module.exports = {
